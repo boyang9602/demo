a7febc283685cb3a687bfd6467f5844c3cec6f17
Bo Yang <boyang9602@gmail.com>
2020-11-02 23:49:57 -0500

    simplify demo

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -6,17 +6,14 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.BodyDeclaration;
 
-import gr.uom.java.xmi.UMLOperation;
-
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UMLOperation targetMethod = null;
 		String fileContent = "public class Demo{}";
 		CompilationUnit cu = StaticJavaParser.parse(fileContent);
 		List<BodyDeclaration> bodyDecls = cu.findAll(BodyDeclaration.class, 
-				decl -> decl.getEnd().get().line < targetMethod.getLocationInfo().getEndLine());
+				decl -> decl.getEnd().get().line < 0);
 	}
 
 }
